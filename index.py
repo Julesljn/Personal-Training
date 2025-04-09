bf8e970093fe8f54bf24d6106cea0425924ab0e0
Julesljn <juleslejeunepro@gmail.com>
2025-04-09 17:33:21 +0200

    add 3 py

--- index.py
+++ index.py
@@ -10,15 +10,15 @@ def timer(func):
     return wrapper
 
 
-# 1 to 1 000 000
 
+# 1 to 1 000 000
 @timer
 def first():
     for x in range(1, 1000001):
         print(x)
 
-# multiplication table from 1 to 20
 
+# multiplication table from 1 to 20
 @timer
 def second():
     for x in range(1, 21):
@@ -27,17 +27,57 @@ def second():
 
 
 # Fibonacci sequence (array)
-
 @timer
 def third(x):
     array = [0, 1]
     for f in range(0, x-1):
         array.append(array[-1] + array[-2])
     print(array[-1])
+
+
+# Integer to binary
+@timer
+def fourth(x):
+    array = []
+    while x > 0:
+        array.append(x % 2)
+        x = x // 2
+    array.reverse()
+    print(''.join(str(bit) for bit in array))
     
-        
-    
-        
+
+# Pascal's triangle
+@timer
+def fifth(n):
+    print([1])
+    last_line = [1, 1]
+    actual_line = []
+    print(last_line)
+
+    for l in range(2, n):
+        actual_line = []
+        for i in range(1, len(last_line)):
+            actual_line.append(last_line[i] + last_line[i-1])
+
+        actual_line = [1] + actual_line + [1]
+        last_line = actual_line        
+        print(actual_line)
+
+
+# Pascal's triangle optimized
+@timer
+def pascal(n):
+    line = [1]
+    max_width = n * 4  # Largeur estimée pour centrer
+
+    print(' '.join(map(str, line)).center(max_width))
+
+    for _ in range(1, n):
+        line = [1] + [line[i] + line[i + 1] for i in range(len(line) - 1)] + [1]
+        print(' '.join(map(str, line)).center(max_width))
+
+
+
 
 
 
@@ -45,4 +85,7 @@ def third(x):
 
 # first()
 # second()
-third(2000)
+# third(20)
+# fourth(13)
+# fifth(1000)
+pascal(20)
